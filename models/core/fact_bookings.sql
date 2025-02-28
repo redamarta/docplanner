33d8141e0e2879021311b178e29a84dfda0dfd95
Marta <marta.reda@payfit.com>
2025-02-28 09:41:53 +0100

    add new changes

--- models/core/fact_bookings.sql
+++ models/core/fact_bookings.sql
@@ -6,14 +6,15 @@
 -- bookings from marketplace
 select
     bookings_log.original_booking_id
-    , .bookings.booking_id
-    , .bookings.calendar_id
-    , .bookings.doctor_id
-    , .bookings.patient_id
-    , .bookings.created_at
-    , .bookings.status
-    , .bookings.source
-    , .bookings.booked_by
+    , bookings.booking_id
+    , bookings.calendar_id
+    , bookings.doctor_id
+    , bookings.patient_id
+    , bookings.created_at
+    , bookings.status
+    , bookings.source
+    , bookings.booked_by
+    , bookings.canceled_at
 from {{ ref('stg_docplanner__bookings') }} as bookings
 left join {{ ref('stg_docplanner__booking_change_log') }} as bookings_log
     on bookings.booking_id = bookings_log.booking_id
@@ -29,15 +30,19 @@ union all
 
 -- booking from external source done by doctor or secretary
 select
-    booking_id
-    , calendar_id
-    , doctor_id
-    , patient_id
-    , created_at
-    , status
-    , source
-    , booked_by
+    bookings_log.original_booking_id
+    , bookings.booking_id
+    , bookings.calendar_id
+    , bookings.doctor_id
+    , bookings.patient_id
+    , bookings.created_at
+    , bookings.status
+    , bookings.source
+    , bookings.booked_by
+    , bookings.canceled_at
 from {{ ref('stg_external__bookings') }} as bookings
+left join {{ ref('stg_docplanner__booking_change_log') }} as bookings_log
+    on bookings.booking_id = bookings_log.booking_id
 {% if is_incremental() %}
 where not exists (
     select null
---
+++ models/staging/docplanner/stg_docplanner__patients.md
@@ -0,0 +1,11 @@
+{% docs stg_docplanner__patients__patient_id %}
+Unique identifier for each patient.
+{% enddocs %}
+
+{% docs stg_docplanner__patients__first_name %}
+Patient's first name.
+{% enddocs %}
+
+{% docs stg_docplanner__patients__last_name %}
+Patient's last name.
+{% enddocs %}
